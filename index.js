c964879524e7e9b3d9e13cff7781fb14d36ecc26
Md Hasanul Karim <hasanlink71@yahoo.com>
2022-11-05 22:11:15 +0600

    order data sheet is ready to fetch and post

--- index.js
+++ index.js
@@ -1,18 +1,13 @@
 const express = require('express');
 const cors = require('cors');
 const app = express();
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const port = process.env.PORT || 5000;
 require('dotenv').config();
 
 //middle wares
 app.use(cors());
 app.use(express.json());
-//dbuser3
-//7UzCUMlgbkH6cxpq
-
-console.log(process.env.DB_USER)
-console.log(process.env.DB_PASSWORD)
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.zjh2ngr.mongodb.net/?retryWrites=true&w=majority`;
 console.log(uri)
@@ -20,7 +15,36 @@ const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology:
 
 async function run() {
   try {
-    const serviceColection = client.db('geniusCar').collection('services');
+    const serviceCollection = client.db('geniusCar').collection('services');
+    const orderCollection = client.db('geniusCar').collection('orders');
+    app.get('/services', async (req, res) => {
+      const query = {}
+      const cursor = serviceCollection.find(query);
+      const services = await cursor.toArray();
+      res.send(services)
+    });
+
+    app.get('/services/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const service = await serviceCollection.findOne(query);
+      res.send(service);
+    });
+
+    //orders api
+    app.get('/orders', async (req, res) => {
+      console.log(req.query)
+      let query = {};
+      const cursor = orderCollection.find(query);
+      const orders = await cursor.toArray();
+      res.send(orders);
+    })
+
+    app.post('/orders', async (req, res) => {
+      const order = req.body;
+      const result = await orderCollection.insertOne(order);
+      res.send(result);
+    })
 
   }
   finally {
