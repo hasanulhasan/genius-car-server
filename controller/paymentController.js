6a340d7e7876dea74fdc8a8e18fd3176c6b63357
hasanulhasan <hasanlink71@yahoo.com>
2023-09-14 14:56:13 +0600

    bkash payment gateway integrated

---
+++ controller/paymentController.js
@@ -0,0 +1,88 @@
+const axios = require("axios");
+// const paymentModel = require('../model/paymentModel')
+const globals = require("node-global-storage");
+const { v4: uuidv4 } = require("uuid");
+
+class paymentController {
+  bkash_headers = async () => {
+    return {
+      "Content-Type": "application/json",
+      Accept: "application/json",
+      authorization: globals.get("id_token"),
+      "x-app-key": process.env.bkash_api_key,
+    };
+  };
+
+  payment_create = async (req, res) => {
+    const { price, email } = req.body;
+    globals.set("userId", email);
+
+    try {
+      const { data } = await axios.post(
+        process.env.bkash_create_payment_url,
+        {
+          mode: "0011",
+          payerReference: " ",
+          callbackURL:
+            "https://genius-car-server-pi-one.vercel.app/api/bkash/payment/callback",
+          amount: price,
+          currency: "BDT",
+          intent: "sale",
+          merchantInvoiceNumber: "Inv" + uuidv4().substring(0, 5),
+        },
+        {
+          headers: await this.bkash_headers(),
+        }
+      );
+
+      // console.log(data)
+      return res.status(200).json({ bkashURL: data.bkashURL });
+    } catch (error) {
+      console.log(error);
+      return res.status(401).json({ error: error.message });
+    }
+  };
+
+  call_back = async (req, res) => {
+    const { paymentID, status } = req.query;
+
+    if (status === "cancel" || status === "failure") {
+      return res.redirect(`https://genius-car-55613.web.app/error?message=${status}`);
+    }
+    if (status === "success") {
+      try {
+        const { data } = await axios.post(
+          process.env.bkash_execute_payment_url,
+          { paymentID },
+          {
+            headers: await this.bkash_headers(),
+          }
+        );
+
+        if (data && data.statusCode === "0000") {
+          //const userId = globals.get('userId')
+          // await paymentModel.create({
+          //     userId: Math.random() * 10 + 1 ,
+          //     paymentID,
+          //     trxID: data.trxID,
+          //     date: data.paymentExecuteTime,
+          //     amount: parseInt(data.amount)
+          // })
+
+          return res.redirect(`https://genius-car-55613.web.app/success`);
+        } else {
+          return res.redirect(
+            `https://genius-car-55613.web.app/error?message=${data.statusMessage}`
+          );
+        }
+      } catch (error) {
+        console.log(error);
+        return res.redirect(
+          `https://genius-car-55613.web.app/error?message=${error.message}`
+        );
+      }
+    }
+  };
+}
+
+module.exports = new paymentController();
--- index.js
+++ index.js
@@ -1,11 +1,11 @@
-const express = require('express');
-const cors = require('cors');
+const express = require("express");
+const cors = require("cors");
 const app = express();
-const jwt = require('jsonwebtoken');
-const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const jwt = require("jsonwebtoken");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const port = process.env.PORT || 5000;
-const SSLCommerzPayment = require('sslcommerz-lts')
-require('dotenv').config();
+const SSLCommerzPayment = require("sslcommerz-lts");
+require("dotenv").config();
 
 //sslcommerz
 const store_id = process.env.STORE_ID;
@@ -16,21 +16,28 @@ const is_live = false; //true for live, false for sandbox
 app.use(cors());
 app.use(express.json());
 
+app.use("/api", require("./routes/routes"));
+
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.zjh2ngr.mongodb.net/?retryWrites=true&w=majority`;
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
 
 async function run() {
   try {
-    const serviceCollection = client.db('geniusCar').collection('services');
-    const orderCollection = client.db('geniusCar').collection('orders');
-    app.get('/services', async (req, res) => {
-      const query = {}
+    const serviceCollection = client.db("geniusCar").collection("services");
+    const orderCollection = client.db("geniusCar").collection("orders");
+
+    app.get("/services", async (req, res) => {
+      const query = {};
       const cursor = serviceCollection.find(query);
       const services = await cursor.toArray();
-      res.send(services)
+      res.send(services);
     });
 
-    app.get('/services/:id', async (req, res) => {
+    app.get("/services/:id", async (req, res) => {
       const id = req.params.id;
       const query = { _id: ObjectId(id) };
       const service = await serviceCollection.findOne(query);
@@ -38,60 +45,61 @@ async function run() {
     });
 
     //orders api
-    app.get('/orders', async (req, res) => {
+    app.get("/orders", async (req, res) => {
       // console.log(req.query.email)
       let query = {};
       if (req.query.email) {
         query = {
-          email: req.query.email
-        }
+          email: req.query.email,
+        };
       }
       const cursor = orderCollection.find(query);
       const orders = await cursor.toArray();
       res.send(orders);
     });
 
-
     //this is order placing
-    app.post('/orders', async (req, res) => {
-      const order = req.body
-      const orderedService = await serviceCollection.findOne({ _id: ObjectId(order.service) })
+    app.post("/orders", async (req, res) => {
+      const order = req.body;
+      const orderedService = await serviceCollection.findOne({
+        _id: ObjectId(order.service),
+      });
       // console.log(orderedService)
       transactionId = new ObjectId().toString();
       const data = {
         total_amount: orderedService.price,
         currency: order.currency,
         tran_id: transactionId, // use unique tran_id for each api call
-        success_url: `http://localhost:5000/payment/success?transactionId=${transactionId}`,
-        fail_url: 'http://localhost:5000/payment/fail',
-        cancel_url: 'http://localhost:5000/payment/cancel',
-        ipn_url: 'http://localhost:3000/ipn',
-        shipping_method: 'Courier',
+        success_url: `https://genius-car-server-pi-one.vercel.app/payment/success?transactionId=${transactionId}`,
+        fail_url: "https://genius-car-server-pi-one.vercel.app/payment/fail",
+        cancel_url: "https://genius-car-server-pi-one.vercel.app/payment/cancel",
+        ipn_url: "https://genius-car-55613.web.app/ipn",
+        shipping_method: "Courier",
         product_name: order.serviceName,
-        product_category: 'Electronic',
-        product_profile: 'general',
+        product_category: "Electronic",
+        product_profile: "general",
         cus_name: order.customerName,
         cus_email: order.email,
         cus_add1: order.address,
-        cus_add2: 'Dhaka',
-        cus_city: 'Dhaka',
-        cus_state: 'Dhaka',
-        cus_postcode: '1000',
-        cus_country: 'Bangladesh',
-        cus_phone: '01711111111',
-        cus_fax: '01711111111',
-        ship_name: 'Customer Name',
-        ship_add1: 'Dhaka',
-        ship_add2: 'Dhaka',
-        ship_city: 'Dhaka',
-        ship_state: 'Dhaka',
+        cus_add2: "Dhaka",
+        cus_city: "Dhaka",
+        cus_state: "Dhaka",
+        cus_postcode: "1000",
+        cus_country: "Bangladesh",
+        cus_phone: order.phone,
+        cus_fax: "01711111111",
+        ship_name: "Customer Name",
+        ship_add1: "Dhaka",
+        ship_add2: "Dhaka",
+        ship_city: "Dhaka",
+        ship_state: "Dhaka",
         ship_postcode: order.postCode,
-        ship_country: 'Bangladesh',
+        ship_country: "Bangladesh",
       };
       // console.log(data)
-      const sslcz = new SSLCommerzPayment(store_id, store_passwd, is_live)
+      const sslcz = new SSLCommerzPayment(store_id, store_passwd, is_live);
 
-      sslcz.init(data).then(apiResponse => {
+      sslcz.init(data).then((apiResponse) => {
         // Redirect the user to payment gateway
         let GatewayPageURL = apiResponse.GatewayPageURL;
         // res.redirect(GatewayPageURL)
@@ -99,67 +107,63 @@ async function run() {
         orderCollection.insertOne({
           ...order,
           transactionId,
-          paid: false
-        })
+          paid: false,
+        });
         res.send({ url: GatewayPageURL });
       });
-
     });
 
-    app.post('/payment/success', async (req, res) => {
+    app.post("/payment/success", async (req, res) => {
       const { transactionId } = req.query;
       const result = await orderCollection.updateOne(
-        { transactionId }, 
-        { $set: { paid: true , paidAt: new Date() } }
-      )
+        { transactionId },
+        { $set: { paid: true, paidAt: new Date() } }
+      );
       if (result.modifiedCount > 0) {
-        res.redirect(`http://localhost:3000/payment/success?transactionId=${transactionId}`)
+        res.redirect(
+          `https://genius-car-55613.web.app/payment/success?transactionId=${transactionId}`
+        );
       }
-    })
-    
-    app.post('/payment/fail', async (req, res) => {
-        res.redirect(`http://localhost:3000/`)
-    })
+    });
 
-    app.get('/orders/by-transaction-id/:id', async (req, res)=> {
+    app.post("/payment/fail", async (req, res) => {
+      res.redirect(`https://genius-car-55613.web.app/`);
+    });
+
+    app.get("/orders/by-transaction-id/:id", async (req, res) => {
       const { id } = req.params;
-      const order = await orderCollection.findOne({transactionId: id});
+      const order = await orderCollection.findOne({ transactionId: id });
       res.send(order);
-    })
+    });
 
-    app.delete('/orders/:id', async (req, res) => {
+    app.delete("/orders/:id", async (req, res) => {
       const id = req.params.id;
       const query = { _id: ObjectId(id) };
       const result = await orderCollection.deleteOne(query);
       res.send(result);
     });
 
-    app.patch('/orders/:id', async (req, res) => {
+    app.patch("/orders/:id", async (req, res) => {
       const id = req.params.id;
-      const status = req.body.status
-      const query = { _id: ObjectId(id) }
+      const status = req.body.status;
+      const query = { _id: ObjectId(id) };
       const updatedDoc = {
         $set: {
-          status: status
-        }
-      }
-      const result = await orderCollection.updateOne(query, updatedDoc)
+          status: status,
+        },
+      };
+      const result = await orderCollection.updateOne(query, updatedDoc);
       res.send(result);
-    })
-
+    });
+  } finally {
   }
-  finally {
-
-  }
-
 }
-run().catch(e => console.error(e))
+run().catch((e) => console.error(e));
 
-
-app.get('/', (req, res) => {
-  res.send('genius car server is running');
-})
+app.get("/", (req, res) => {
+  res.send("genius car server is running");
+});
 
 app.listen(port, () => {
-  console.log(`Genius Car service on ${port}`)
-})
+  console.log(`Genius Car service on ${port}`);
+});
